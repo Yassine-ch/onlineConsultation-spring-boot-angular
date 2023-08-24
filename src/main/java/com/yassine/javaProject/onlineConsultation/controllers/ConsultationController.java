a873f69823c23f5060d258b2fdbe2be00ee4d6f4
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-08-24 16:55:55 +0100

    ConsultationController

--- src/main/java/com/yassine/javaProject/onlineConsultation/controllers/ConsultationController.java
+++ src/main/java/com/yassine/javaProject/onlineConsultation/controllers/ConsultationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") //To Link with Angular
 @RequestMapping("/api/consultations")
 public class ConsultationController {
 
