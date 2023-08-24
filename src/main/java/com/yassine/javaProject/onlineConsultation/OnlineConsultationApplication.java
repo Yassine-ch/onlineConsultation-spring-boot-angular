c0b1f2445eaaa06450b031c1a566f9cf628bac7b
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-08-24 11:40:56 +0100

    first

---
+++ src/main/java/com/yassine/javaProject/onlineConsultation/OnlineConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.yassine.javaProject.onlineConsultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineConsultationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineConsultationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yassine/javaProject/onlineConsultation/OnlineConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassine.javaProject.onlineConsultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineConsultationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
