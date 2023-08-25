67aed4c192223d3f1b627c50ab901c30a5e8ac20
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-08-25 16:59:23 +0100

    update

--- src/main/java/com/yassine/javaProject/onlineConsultation/repositories/PatientRepository.java
+++ src/main/java/com/yassine/javaProject/onlineConsultation/repositories/PatientRepository.java
@@ -2,13 +2,11 @@ package com.yassine.javaProject.onlineConsultation.repositories;
 
 import com.yassine.javaProject.onlineConsultation.models.Patient;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface PatientRepository extends CrudRepository<Patient,Long> {
-    //FIND All
-
-    @Override
-    List<Patient> findAll();
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Patient findByEmail(String email);
 }
--- src/main/java/com/yassine/javaProject/onlineConsultation/services/PatientService.java
+++ src/main/java/com/yassine/javaProject/onlineConsultation/services/PatientService.java
@@ -1,4 +1,37 @@
 package com.yassine.javaProject.onlineConsultation.services;
 
-public class PatientService {
+import com.yassine.javaProject.onlineConsultation.models.Patient;
+import com.yassine.javaProject.onlineConsultation.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class PatientService implements UserDetailsService {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Patient save(Patient patient) {
+        patient.setPassword(bCryptPasswordEncoder.encode(patient.getPassword()));
+        return patientRepository.save(patient);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Patient patient = patientRepository.findByEmail(email);
+        if (patient == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return new User(patient.getEmail(), patient.getPassword(), new ArrayList<>());
+    }
 }
